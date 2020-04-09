c6ce6351133b5e3fadda6170d0186fb8366f6c3a
Alcee Metevier <almetevier@gmail.com>
2020-04-09 16:57:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,28 +29,28 @@ function ternaryTeenager(age) {
 var age = 15;
 function switchAge(age) {
   switch(age) {
-  case (13):
-  console.log("You are a teenager");
-  break;
-  case (14):
-  console.log("You are a teenager");
-  break;
-  case (15):
-  console.log("You are a teenager");
-  break;
-  case (16):
-  console.log("You are a teenager");
-  break;
-  case (17):
-  console.log("You are a teenager");
-  break;
-  case (18):
-  console.log("You are a teenager");
-  break;
-  case (19):
-  console.log("You are a teenager");
-  break;
-  default:
-  console.log("You have an age");
+    case (13):
+      console.log("You are a teenager");
+      break;
+    case (14):
+      console.log("You are a teenager");
+      break;
+    case (15):
+      console.log("You are a teenager");
+      break;
+    case (16):
+      console.log("You are a teenager");
+      break;
+    case (17):
+      console.log("You are a teenager");
+      break;
+    case (18):
+      console.log("You are a teenager");
+      break;
+    case (19):
+      console.log("You are a teenager");
+      break;
+    default:
+      console.log("You have an age");
   }
 }
